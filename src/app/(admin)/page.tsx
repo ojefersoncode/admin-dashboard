f264db1a0d89d8f89e8ef2516800b9aaf16c7a92
ojefersoncode <Jefersonvidal999@gmail.com>
2025-07-30 10:07:34 -0300

    Edit home

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { EcommerceMetrics } from "@/components/ecommerce/EcommerceMetrics";
 import React from "react";
 import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
 import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
-import StatisticsChart from "@/components/ecommerce/StatisticsChart";
 import RecentOrders from "@/components/ecommerce/RecentOrders";
 import DemographicCard from "@/components/ecommerce/DemographicCard";
 import NavBar from "@/components/header/Navbar";
@@ -18,23 +16,16 @@ export default function Ecommerce() {
   return (
     <div className="flex flex-col gap-4 md:gap-6">
       <div className="w-full">
-          <NavBar />
+        <NavBar />
       </div>
-    
-      
-      <div className="col-span-12 space-y-6 xl:col-span-7">
-        <EcommerceMetrics />
+
+
+      <div className="flex flex-wrap items-center gap-4">
 
         <MonthlySalesChart />
-      </div>
-
-      <div className="col-span-12 xl:col-span-5">
         <MonthlyTarget />
       </div>
 
-      <div className="col-span-12">
-        <StatisticsChart />
-      </div>
 
       <div className="col-span-12 xl:col-span-5">
         <DemographicCard />
