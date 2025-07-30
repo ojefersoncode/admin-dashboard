835854b2b5ddb500dd996e9310fc071dd44a254c
ojefersoncode <Jefersonvidal999@gmail.com>
2025-07-30 09:29:47 -0300

    Removed page not used

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -16,6 +16,7 @@ export const metadata: Metadata = {
 export default function Ecommerce() {
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
+      
       <div className="col-span-12 space-y-6 xl:col-span-7">
         <EcommerceMetrics />
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Outfit } from 'next/font/google';
 import './globals.css';
 
-import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
 
 const outfit = Outfit({
@@ -17,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${outfit.className} dark:bg-gray-900`}>
         <ThemeProvider>
-          <SidebarProvider>{children}</SidebarProvider>
+          {children}
         </ThemeProvider>
       </body>
     </html>
